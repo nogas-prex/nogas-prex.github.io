0e3196696afc92e04e6b68c833716adfd91acbe7
chenhaozhu2021 <chenhaozhu2019@gmail.com>
2022-09-29 12:37:00 -0400

    Font Change (Doesn't Work)

--- app/nogas/src/Theme.js
+++ app/nogas/src/Theme.js
@@ -1,57 +1,59 @@
 import { createTheme } from '@mui/material';
 import BAHNSCHRIFT from './fonts/BAHNSCHRIFT 1.TTF'
 import BAHNSCHRIFT2 from "./fonts/BAHNSCHRIFT 1.TTF";
+import AtlasGroteskBold from "./fonts/AtlasGroteskBold.otf";
 
-const banschrift = {
-  src: `
-    local('BAHNSCHRIFT'),
-    url(${BAHNSCHRIFT2}) format('ttf')
+
+const atlasGroteskBold = {
+    src: `
+    local('AtlasGroteskBold'),
+    url(${AtlasGroteskBold}) format('otf')
   `,
 };
 
 export const NOGASTheme = createTheme({
-  palette: {
-    type: "light",
-    primary: {
-      main: "#00A360",
+    palette: {
+        type: "light",
+        primary: {
+            main: "#00A360",
+        },
+        secondary: {
+            main: "#696969",
+        },
+        text: {
+            secondary: "#ffffff",
+        },
     },
-    secondary: {
-      main: "#696969",
+    typography: {
+        fontFamily: ["AtlasGroteskBold"],
+        subtitle: {
+            fontSize: "3em",
+        },
+        h1: {
+            fontFace: [AtlasGroteskBold],
+        },
+        h4: {
+            textDecoration: "underline",
+            fontWeight: 700,
+        },
+        body1: {
+            textAlign: "justify",
+            fontSize: "1.5em",
+            color: "white",
+        },
+        body2: {
+            textAlign: "justify",
+            fontSize: "1.5em",
+            color: "black",
+        },
+        body3: {
+            fontSize: "1.1em",
+            color: "black",
+        },
+        h3White: {
+            textAlign: "justify",
+            fontSize: "1.5em",
+            color: "white",
+        },
     },
-    text: {
-      secondary: "#ffffff",
-    },
-  },
-  typography: {
-    fontFamily: ["Bahnschrift"],
-    subtitle: {
-      fontSize: "3em",
-    },
-    h1: {
-      fontFace: [banschrift],
-    },
-    h4: {
-      textDecoration: "underline",
-      fontWeight: 700,
-    },
-    body1: {
-      textAlign: "justify",
-      fontSize: "1.5em",
-      color: "white",
-    },
-    body2: {
-      textAlign: "justify",
-      fontSize: "1.5em",
-      color: "black",
-    },
-    body3: {
-      fontSize: "1.1em",
-      color: "black",
-    },
-    h3White: {
-      textAlign: "justify",
-      fontSize: "1.5em",
-      color: "white",
-    },
-  },
 });
