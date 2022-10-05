f38da2c06a07f2e303b2a18afa800184c0437edc
chenhaozhu2021 <chenhaozhu2019@gmail.com>
2022-10-05 12:34:39 -0400

    Add Text
    Add updated text for five case studies

--- app/nogas/src/views/case-studies/FutureOfWork.js
+++ app/nogas/src/views/case-studies/FutureOfWork.js
@@ -1,109 +1,131 @@
+import {
+    CardContent,
+    CardHeader,
+    CardMedia,
+    Grid,
+    Typography,
+} from "@mui/material";
+
 import ResponsiveAppBar from "../../components/AppBar";
-import CaseStudyVision from "../../components/caseStudies/caseStudyVision";
-import CaseStudyLanding from "../../components/caseStudies/caseStudyLanding";
 import CaseStudyBackground from "../../components/caseStudies/caseStudyBackground";
-import { Grid } from "@mui/material";
+import CaseStudyLanding from "../../components/caseStudies/caseStudyLanding";
+import CaseStudyVision from "../../components/caseStudies/caseStudyVision";
 import ImageSlider from "../../components/ImageSlider";
+
+import headerImage from "../../img/4_Case Study 02_ Preparing for Future Growth/0_Cover/1.png";
+import backgroundImage from "../../img/4_Case Study 02_ Preparing for Future Growth/2_Background/0_Cover/1.jpg";
+import beforeImage from '../../img/4_Case Study 02_ Preparing for Future Growth/3_Vision/Slider_Before.png'
+import afterImage from "../../img/4_Case Study 02_ Preparing for Future Growth/3_Vision/Slider_After.png";
+import densityImage from '../../img/4_Case Study 02_ Preparing for Future Growth/3_Vision/Smart Density.png'
+import vehUsageImage from "../../img/4_Case Study 02_ Preparing for Future Growth/3_Vision/Vehicle Usage.png";
+import envPerfImage from "../../img/4_Case Study 02_ Preparing for Future Growth/3_Vision/Better Envir-performance.png";
 import CaseStudySummary from "../../components/caseStudies/caseStudySummary";
-import headerImage from '../../img/4_Case Study 02_ Preparing for Future Growth/0_Cover/1.png'
+import carImage from '../../img/4_Case Study 02_ Preparing for Future Growth/1_Metrics/Vehicle Trips.png'
+import pedImage from "../../img/4_Case Study 02_ Preparing for Future Growth/1_Metrics/Pedestrian Accessibility.png";
+import permImage from "../../img/4_Case Study 02_ Preparing for Future Growth/1_Metrics/Permeable.png";
+import canopyImage from "../../img/4_Case Study 02_ Preparing for Future Growth/1_Metrics/Canopy.png";
 
 
 export default function FutureOfWork() {
-  return (
-    <div>
-      <ResponsiveAppBar />
-      <CaseStudyLanding
-        image={headerImage}
-        title="Layering Sustainability"
-        subtitle="Landscape Optimization"
-        body={[
-          `Metro areas are searching harder than ever for solutions to mitigate the effects of
-            climate change such as extreme heat, drought, flooding, and wildfire. Meanwhile, more
-            people are moving in than out of areas facing high risk from climate change, which
-            accelerates the urbanization rate in these areas. As the result of urbanization processes
-            – more permeable surfaces being paved over. Massive impervious surface further
-            exacerbates the extreme heat, urban flooding, and other climate disasters. Hence, a
-            vicious circle is formed.`,
-          `second paragraph like this`,
-        ]}
-      />
+    return ( <
+        div >
+        <
+        ResponsiveAppBar / >
+        <
+        CaseStudyLanding image = { headerImage }
+        title = "DESIGNING THE FUTURE OF WORK"
+        subtitle = "Employment Campus Evolution"
+        body = {
+            [
+                `Since the birth of the suburban employment campus in the 1940s, these developments have provided large, 
+                high-quality, efficient workspaces for big corporations. Today, according to the U.S. Bureau of Labor Statistics, 
+                32 percent of U.S. employment is in the suburbs of large metropolitan areas. However, as people and jobs continue 
+                to suburbanize, the distance between people and jobs is growing, and the geography between people and jobs is changing. `,
+                `The future of work is less certain than ever since the COVID-19 pandemic disrupted conventional working modes. Remote 
+                working or working from home is becoming more popular and necessary than ever. The suburb-to-suburb commuting challenge, 
+                the evolving remote work options, and proliferation of autonomous mobility and micromobility provide a key opportunity to 
+                reimagine the typical suburban employment campus.  `,
+            ]
+        }
+        />
 
-      <CaseStudyBackground
-        title="Background"
-        subtitle=""
-        image={""}
-        body={[``]}
-        stats={[
-          { pct: "1876", text: "Anual Household Vehicle Trips" },
-          {
-            pct: "25%+",
-            text: "Residential Land Devoted to Vehicular Surfaces",
-          },
-          { pct: "35%+", text: "GHG Emissions Reduction Potential" },
-        ]}
-      />
+        <
+        CaseStudyBackground title = "BACKGROUND"
+        subtitle = "Outdated Employment Campus Design"
+        image = { "" }
+        body = {
+            [
+                `The design of suburban campuses themselves hasn’t evolved much over the past several decades. The 
+                footprint of parking lots designed to support these campuses is often several times larger than the 
+                office buildings, which not only wastes valuable land, but also causes larger-scale environmental degradation.`,
+            ]
+        }
+        stats = {
+            [
+                { pct: "1876", text: "Anual Household Vehicle Trips" },
+                {
+                    pct: "25%+",
+                    text: "Residential Land Devoted to Vehicular Surfaces",
+                },
+                { pct: "35%+", text: "GHG Emissions Reduction Potential" },
+            ]
+        }
+        />
 
-      <Grid
-        container
-        sx={{ width: "100vw", height: "70vh", overflowY: "hidden" }}
-      >
-        <ImageSlider leftImage={""} rightImage={""} />
-      </Grid>
-      <CaseStudyVision
-        title="Vision"
-        subtitle="The Future of Autonomous Suburbs"
-        body={[
-          `In the near term, a more integrative approach to zoning
-        can help redistribute smaller commercial clusters
-        and increased park space throughout residential
-        neighborhoods, allowing a finer-grained suburban
-        fabric to emerge that caters more explicitly to a mix of
-        pedestrians, and micro-transit users. This atomization
-        of residential islands, when applied to greenfield
-        development, can facilitate a significant reduction of
-        long-distance vehicular trips by clustering land uses
-        together within a much smaller walk shed. It can also
-        increase the environmental benefits of heterogeneity
-        by expanding access to public open space areas, which
-        has proven to be one of the most valuable neighborhood
-        public health assets since the beginning of the
-        COVID-19 pandemic.`,
-          `A transition from traditional car-based urbanism to
-        autonomous, on-demand mobility paradigms will further
-        support a shift towards finer-grained land use planning
-        by leveraging new micro-mobility options to grant users
-        greater access to services and amenities.`,
-        ]}
-        icons={[
-          { img: "", alt: "", label: "Smart Density" },
-          { img: "", alt: "", label: "Reduced Vehicle Usage" },
-          {
-            img: "",
-            alt: "",
-            label: "Better Environmental Performance",
-          },
-        ]}
-      />
+        <
+        Grid container sx = {
+            { width: "100vw", height: "70vh", overflowY: "hidden" }
+        } >
+        <
+        ImageSlider leftImage = { "" }
+        rightImage = { "" }
+        /> < /
+        Grid > <
+        CaseStudyVision title = "VISION"
+        subtitle = "The Future of Autonomous Suburbs"
+        body = {
+            [
+                `In the near term, evolving remote work options, especially hybrid working modes, will contribute to increased demands for smaller, 
+                more flexible workspaces. Meanwhile, fewer workers on-site at the same time will need fewer parking spaces than before, creating 
+                opportunities to convert large parking areas to housing or to enhance a site’s environmental performance. `,
+                `In the autonomous future, with complete adoption of autonomous mobility services, on-site parking can be further reduced or even eliminated. `,
+            ]
+        }
+        icons = {
+            [
+                { img: "", alt: "", label: "Smart Density" },
+                { img: "", alt: "", label: "Reduced Vehicle Usage" },
+                {
+                    img: "",
+                    alt: "",
+                    label: "Better Environmental Performance",
+                },
+            ]
+        }
+        />
 
-      <div>custom stuff</div>
+        <
+        div > custom stuff < /div>
 
-      <CaseStudySummary
-        text={``}
-        icons={[
-          { img: "", pct: "50%", text: "Reduction of vehicle tripps" },
-          {
-            img: "",
-            pct: "60%",
-            text: "Increase in pedestrian accessibility",
-          },
-          {
-            img: "",
-            pct: "80%",
-            text: "Reduction of impervious surface cover",
-          },
-          { img: "", pct: "60%", text: "Increase in tree canopy" },
-        ]}
-      />
-    </div>
-  );
+        <
+        CaseStudySummary text = { `` }
+        icons = {
+            [
+                { img: "", pct: "50%", text: "Reduction of vehicle tripps" },
+                {
+                    img: "",
+                    pct: "60%",
+                    text: "Increase in pedestrian accessibility",
+                },
+                {
+                    img: "",
+                    pct: "80%",
+                    text: "Reduction of impervious surface cover",
+                },
+                { img: "", pct: "60%", text: "Increase in tree canopy" },
+            ]
+        }
+        /> < /
+        div >
+    );
 }
--- app/nogas/src/views/case-studies/PrepareFutureGrowth.js
+++ app/nogas/src/views/case-studies/PrepareFutureGrowth.js
@@ -1,9 +1,9 @@
 import {
-  CardContent,
-  CardHeader,
-  CardMedia,
-  Grid,
-  Typography,
+    CardContent,
+    CardHeader,
+    CardMedia,
+    Grid,
+    Typography,
 } from "@mui/material";
 
 import ResponsiveAppBar from "../../components/AppBar";
@@ -27,110 +27,115 @@ import canopyImage from "../../img/4_Case Study 02_ Preparing for Future Growth/
 
 
 export default function PrepareFutureGrowth() {
-  return (
-    <div>
-      <ResponsiveAppBar />
-      <CaseStudyLanding
-        image={headerImage}
-        title="Preparing for Future Growth"
-        subtitle="Single Family Residential Land Use Optimization"
-        body={[
-          `Today, around 75% of residential suburban areas are zoned for single-family housing.
-            This has resulted in highly standardized, car-dependent districts and exclusive
-            enclaves, comprised of large contiguous residential islands with minimal distribution
-            of public open space, bounded by high-volume, commercial and retail corridors. This
-            configuration increases the burden on municipal infrastructure and often requires the
-            construction of costly transportation systems and other utilities to support population
-            growth.`,
-          `Three big factors are determining the need for radically new types of suburban form:
-            the proliferation of AEVs, the rapid adoption of post-COVID work from home scenarios
-            (WFH), and the massive population of new Millennial homebuyers. Together, these
-            three forces are likely to upend many of the fundamental assumptions that have guided
-            suburban land use planning over the past century. New residential planning principles
-            aimed at increasing social equity, environmental performance, health benefits, and
-            overall access to new mobility paradigms, are desperately needed.
+    return ( <
+        div >
+        <
+        ResponsiveAppBar / >
+        <
+        CaseStudyLanding image = { headerImage }
+        title = "PREPARING FOR FUTURE GROWTH"
+        subtitle = "Single Family Residential Land Use Optimization"
+        body = {
+            [
+                `Today, around 75 percent of residential suburban areas are zoned for single-family 
+                housing. This has resulted in highly standardized, car-dependent districts, and exclusive 
+                enclaves comprised of large contiguous residential islands with minimal distribution of 
+                public open space, bounded by high-volume, commercial and retail corridors. This configuration 
+                increases the burden on municipal infrastructure and often requires the construction of 
+                costly transportation systems and other utilities to support population growth. `,
+                `Three major forces are determining the need for radically new types of suburban form: the 
+                proliferation of alternative energy vehicles, the rapid adoption of post-COVID-19 work-from-home 
+                scenarios, and the massive population of new millennial homebuyers. Together, these three forces 
+                are likely to upend many of the fundamental assumptions that have guided suburban land use planning 
+                over the past century. New residential planning principles aimed at increasing social equity, 
+                environmental performance, health benefits, and overall access to new mobility paradigms are desperately needed. 
             `,
-        ]}
-      />
+            ]
+        }
+        />
 
-      <CaseStudyBackground
-        title="Background"
-        subtitle="Car-based Single Family Residential"
-        image={backgroundImage}
-        body={[
-          `On average, vehicular surfaces (roadways, driveways,
-parking areas, etc.) account for almost 30% of singlefamily
-residential areas. Amenities like retail centers
-and public open space areas are often located outside
-of residential districts, which forces residents to make
-multiple daily household trips for shopping, errands, and
-other recreational purposes. This car-dependency puts
-disproportionate burden on working-class households
-and disabled residents.`,
-        ]}
-        stats={[
-          { pct: "1876", text: "Anual Household Vehicle Trips" },
-          {
-            pct: "25%+",
-            text: "Residential Land Devoted to Vehicular Surfaces",
-          },
-          { pct: "35%+", text: "GHG Emissions Reduction Potential" },
-        ]}
-      />
+        <
+        CaseStudyBackground title = "BACKGROUND"
+        subtitle = "Car-based Single Family Residential"
+        image = { backgroundImage }
+        body = {
+            [
+                `On average, vehicular surfaces (roadways, driveways,
+                parking areas, etc.) account for almost 30% of singlefamily
+                residential areas. Amenities like retail centers
+                and public open space areas are often located outside
+                of residential districts, which forces residents to make
+                multiple daily household trips for shopping, errands, and
+                other recreational purposes. This car-dependency puts
+                disproportionate burden on working-class households
+                and disabled residents.`,
+            ]
+        }
+        stats = {
+            [
+                { pct: "1876", text: "Anual Household Vehicle Trips" },
+                {
+                    pct: "25%+",
+                    text: "Residential Land Devoted to Vehicular Surfaces",
+                },
+                { pct: "35%+", text: "GHG Emissions Reduction Potential" },
+            ]
+        }
+        />
 
-      <Grid
-        container
-        sx={{ width: "100vw", height: "70vh", overflowY: "hidden" }}
-      >
-        <ImageSlider leftImage={beforeImage} rightImage={afterImage} />
-      </Grid>
-      <CaseStudyVision
-        title="Vision"
-        subtitle="The Future of Autonomous Suburbs"
-        body={[
-        `In the near term, a more integrative approach to zoning
-        can help redistribute smaller commercial clusters
-        and increased park space throughout residential
-        neighborhoods, allowing a finer-grained suburban
-        fabric to emerge that caters more explicitly to a mix of
-        pedestrians, and micro-transit users. This atomization
-        of residential islands, when applied to greenfield
-        development, can facilitate a significant reduction of
-        long-distance vehicular trips by clustering land uses
-        together within a much smaller walk shed. It can also
-        increase the environmental benefits of heterogeneity
-        by expanding access to public open space areas, which
-        has proven to be one of the most valuable neighborhood
-        public health assets since the beginning of the
-        COVID-19 pandemic.`,
-        `A transition from traditional car-based urbanism to
-        autonomous, on-demand mobility paradigms will further
-        support a shift towards finer-grained land use planning
-        by leveraging new micro-mobility options to grant users
-        greater access to services and amenities.`,
-        ]}
-        icons={[
-          { img: densityImage, alt: "", label: "Smart Density" },
-          { img: vehUsageImage, alt: "", label: "Reduced Vehicle Usage" },
-          {
-            img: envPerfImage,
-            alt: "",
-            label: "Better Environmental Performance",
-          },
-        ]}
-      />
+        <
+        Grid container sx = {
+            { width: "100vw", height: "70vh", overflowY: "hidden" }
+        } >
+        <
+        ImageSlider leftImage = { beforeImage }
+        rightImage = { afterImage }
+        /> < /
+        Grid > <
+        CaseStudyVision title = "VISION"
+        subtitle = "The Future of Autonomous Suburbs"
+        body = {
+            [
+                `In the near term, a more integrative approach to zoning can help redistribute 
+                smaller commercial clusters and increase park space throughout residential neighborhoods, 
+                allowing a finer-grained suburban fabric to emerge that caters more explicitly to a mix 
+                of pedestrians and microtransit users. This atomization of residential islands, when applied 
+                to greenfield development, can facilitate a significant reduction of long-distance vehicular 
+                trips by clustering land uses together within a much smaller walkshed. It can also increase 
+                the environmental benefits of heterogeneity by expanding access to public open space, which 
+                proved to be one of the most valuable neighborhood public health assets during the COVID-19 pandemic.  `,
+                `A transition from traditional car-based urbanism to autonomous, on-demand mobility paradigms will 
+                further support a shift towards finer-grained land use planning by leveraging micromobility options 
+                to grant residents greater access to services and amenities.`,
+            ]
+        }
+        icons = {
+            [
+                { img: densityImage, alt: "", label: "Smart Density" },
+                { img: vehUsageImage, alt: "", label: "Reduced Vehicle Usage" },
+                {
+                    img: envPerfImage,
+                    alt: "",
+                    label: "Better Environmental Performance",
+                },
+            ]
+        }
+        />
 
-      <div>custom stuff</div>
+        <
+        div > custom stuff < /div>
 
-      <CaseStudySummary
-        text={``}
-        icons={[
-          { img: carImage, pct: "50%", text: "Reduction of vehicle tripps" },
-          { img: pedImage, pct: "60%", text: "Increase in pedestrian accessibility" },
-          { img: permImage, pct: "80%", text: "Reduction of impervious surface cover" },
-          { img: canopyImage, pct: "60%", text: "Increase in tree canopy" },
-        ]}
-      />
-    </div>
-  );
+        <
+        CaseStudySummary text = { `` }
+        icons = {
+            [
+                { img: carImage, pct: "50%", text: "Reduction of vehicle tripps" },
+                { img: pedImage, pct: "60%", text: "Increase in pedestrian accessibility" },
+                { img: permImage, pct: "80%", text: "Reduction of impervious surface cover" },
+                { img: canopyImage, pct: "60%", text: "Increase in tree canopy" },
+            ]
+        }
+        /> < /
+        div >
+    );
 }
